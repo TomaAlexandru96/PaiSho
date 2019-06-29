98705f7ddc57e0cb4b8ab69870d11294eb3a3e6b
Alexandru Toma <bartimaeus1073@gmail.com>
2019-06-29 19:39:17 +0100

    Initial

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject pieces;
+
+    public void PlacePiece(Piece piece, Transform tile)
+    {
+        Piece p = Instantiate(piece, tile.transform.position, Quaternion.identity);
+        p.transform.SetParent(pieces.transform);
+        p.transform.localScale = Vector3.one;
+    }
+}
---
+++ Assets/Scripts/Pieces/Piece.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public enum Colour
+    {
+        Black, White
+    }
+
+    public GameObject blackOutline;
+    public GameObject whiteOutline;
+
+    public Colour PieceColour
+    {
+        get
+        {
+            return blackOutline.activeSelf ? Colour.Black : Colour.White;
+        }
+
+        set
+        {
+            blackOutline.SetActive(value == Colour.Black);
+            whiteOutline.SetActive(value == Colour.White);
+        }
+    }
+
+    public virtual List<Vector2Int> GetPossibleMoves()
+    {
+        return new List<Vector2Int>();
+    }
+}
